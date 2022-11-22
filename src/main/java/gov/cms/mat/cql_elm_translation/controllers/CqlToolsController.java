eec6e2b5c86fdf87e027bd50827957a5a5c76849
RohitKandimalla <57495404+RohitKandimalla@users.noreply.github.com>
2022-11-22 15:56:47 -0500

    Update src/main/java/gov/cms/mat/cql_elm_translation/controllers/CqlToolsController.java
    Co-authored-by: Joe Kotanchik <56264529+jkotanchik-SB@users.noreply.github.com>

--- src/main/java/gov/cms/mat/cql_elm_translation/controllers/CqlToolsController.java
+++ src/main/java/gov/cms/mat/cql_elm_translation/controllers/CqlToolsController.java
@@ -20,8 +20,7 @@ public class CqlToolsController {
 
   @PutMapping("/cql/format")
   public ResponseEntity<String> formatCql(@RequestBody String cqlData, Principal principal) {
-    var cqlDataStream = new ByteArrayInputStream(cqlData.getBytes());
-    try {
+try (var cqlDataStream = new ByteArrayInputStream(cqlData.getBytes())) {
       CqlFormatterVisitor.FormatResult formatResult =
           CqlFormatterVisitor.getFormattedOutput(cqlDataStream);
       if (formatResult.getErrors() != null && !formatResult.getErrors().isEmpty()) {
