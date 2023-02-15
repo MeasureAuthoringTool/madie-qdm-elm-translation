0a9482f35876b0ff6b07db318394459512668a22
Rohit Kandimalla <rohit.kandimalla@semanticbits.com>
2023-02-15 16:52:15 -0500

    Generated Effective Data Requirements and added to bundle for HR to work

--- src/main/java/gov/cms/mat/cql_elm_translation/exceptions/ResourceNotFoundException.java
+++ src/main/java/gov/cms/mat/cql_elm_translation/exceptions/ResourceNotFoundException.java
@@ -3,7 +3,7 @@ package gov.cms.mat.cql_elm_translation.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
   private static final String MESSAGE = "Could not find %s resource for measure: %s";
 
--- src/main/java/gov/cms/mat/cql_elm_translation/service/HumanReadableService.java
+++ src/main/java/gov/cms/mat/cql_elm_translation/service/HumanReadableService.java
@@ -2,6 +2,9 @@ package gov.cms.mat.cql_elm_translation.service;
 
 import ca.uhn.fhir.context.FhirContext;
 import gov.cms.madie.models.measure.Measure;
+import gov.cms.mat.cql_elm_translation.cql_translator.TranslationResource;
+
+import gov.cms.mat.cql_elm_translation.data.RequestData;
 import gov.cms.mat.cql_elm_translation.exceptions.HumanReadableGenerationException;
 import gov.cms.mat.cql_elm_translation.exceptions.ResourceNotFoundException;
 import gov.cms.mat.cql_elm_translation.utils.ResourceUtils;
@@ -9,23 +12,41 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.cqframework.cql.cql2elm.CqlTranslatorOptions;
+import org.cqframework.cql.cql2elm.LibraryManager;
+import org.cqframework.cql.cql2elm.CqlTranslator;
+import org.cqframework.cql.cql2elm.model.CompiledLibrary;
+import org.cqframework.cql.elm.requirements.fhir.DataRequirementsProcessor;
 import org.hl7.fhir.convertors.advisors.impl.BaseAdvisor_40_50;
 import org.hl7.fhir.convertors.conv40_50.VersionConvertor_40_50;
 import org.hl7.fhir.exceptions.FHIRException;
+import org.hl7.fhir.r4.model.Attachment;
 import org.hl7.fhir.r4.model.Bundle;
+import org.hl7.fhir.r5.model.Extension;
+import org.hl7.fhir.r4.model.Library;
 import org.hl7.fhir.r4.model.Resource;
 import org.hl7.fhir.r5.test.utils.TestingUtilities;
 import org.hl7.fhir.r5.utils.LiquidEngine;
 import org.springframework.stereotype.Service;
 
+import java.io.ByteArrayInputStream;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class HumanReadableService {
+public class HumanReadableService extends ResourceUtils {
   private final FhirContext fhirContext;
   private final MadieFhirServices madieFhirServices;
+
+  private final CqlConversionService cqlConversionService;
+
+  private static final String EFFECTIVE_DATA_REQUIREMENT_URL =
+      "http://hl7.org/fhir/us/cqfmeasures/StructureDefinition/cqfm-effectiveDataRequirements";
 
   public String generateHumanReadable(Measure madieMeasure, String accessToken) {
     String fhirMeasureBundle = madieFhirServices.getFhirMeasureBundle(madieMeasure, accessToken);
@@ -36,24 +57,32 @@ public class HumanReadableService {
         throw new ResourceNotFoundException("Measure", madieMeasure.getId());
       }
       try {
-        Optional<Bundle.BundleEntryComponent> bundleEntryComponent =
-            bundleResource.getEntry().stream()
-                .filter(
-                    entry ->
-                        StringUtils.equalsIgnoreCase(
-                            "Measure", entry.getResource().getResourceType().toString()))
-                .findFirst();
-        if (bundleEntryComponent.isEmpty()) {
+        Optional<Bundle.BundleEntryComponent> measureBundleEntryComponent =
+            getMeasureEntry(bundleResource);
+        if (measureBundleEntryComponent.isEmpty()) {
           log.error("Unable to find BundleEntryComponent for measure {}", madieMeasure.getId());
           throw new ResourceNotFoundException("Measure", madieMeasure.getId());
         }
-        Resource measureResource = bundleEntryComponent.get().getResource();
+        Resource measureResource = measureBundleEntryComponent.get().getResource();
+
+        Optional<Bundle.BundleEntryComponent> libraryBundleEntryComponent =
+            getMeasureLibraryEntry(bundleResource, madieMeasure);
+        if (libraryBundleEntryComponent.isEmpty()) {
+          log.error("Unable to find library for measure {}", madieMeasure.getId());
+          throw new ResourceNotFoundException("Library", madieMeasure.getId());
+        }
+        Library library = (Library) libraryBundleEntryComponent.get().getResource();
 
         // converting measure resource from R4 to R5
-        VersionConvertor_40_50 versionConvertor_40_50 =
-            new VersionConvertor_40_50(new BaseAdvisor_40_50());
+        var versionConvertor_40_50 = new VersionConvertor_40_50(new BaseAdvisor_40_50());
         org.hl7.fhir.r5.model.Measure r5Measure =
             (org.hl7.fhir.r5.model.Measure) versionConvertor_40_50.convertResource(measureResource);
+
+        org.hl7.fhir.r5.model.Library effectiveDataRequirements =
+            getEffectiveDataRequirements(r5Measure, library, accessToken);
+        r5Measure.addContained(effectiveDataRequirements);
+        r5Measure.getExtension().add(createExtension());
+
         String template = ResourceUtils.getData("/templates/Measure.liquid");
         // TODO: Need to write our own implementation of TestingUtilities.getWorkerContext
         LiquidEngine engine = new LiquidEngine(TestingUtilities.context(), null);
@@ -77,5 +106,128 @@ public class HumanReadableService {
       return null;
     }
     return fhirContext.newJsonParser().parseResource(clazz, json);
+  }
+
+  private RequestData createDefaultRequestData() {
+    return RequestData.builder()
+        .showWarnings(false)
+        .annotations(true)
+        .locators(true)
+        .disableListDemotion(true)
+        .disableListPromotion(true)
+        .disableMethodInvocation(false)
+        .validateUnits(true)
+        .resultTypes(true)
+        .build();
+  }
+
+  /**
+   * @param bundleResource Bundle resource
+   * @return BundleEntry which is of type Measure
+   */
+  private Optional<Bundle.BundleEntryComponent> getMeasureEntry(Bundle bundleResource) {
+    return bundleResource.getEntry().stream()
+        .filter(
+            entry ->
+                StringUtils.equalsIgnoreCase(
+                    "Measure", entry.getResource().getResourceType().toString()))
+        .findFirst();
+  }
+
+  /**
+   * @param bundleResource Bundle resource
+   * @param madieMeasure madie measure
+   * @return BundleEntry which is a Measure library
+   */
+  private Optional<Bundle.BundleEntryComponent> getMeasureLibraryEntry(
+      Bundle bundleResource, Measure madieMeasure) {
+    return bundleResource.getEntry().stream()
+        .filter(
+            entry ->
+                StringUtils.equalsIgnoreCase(
+                        "Library", entry.getResource().getResourceType().toString())
+                    && StringUtils.equalsIgnoreCase(
+                        "Library/" + madieMeasure.getCqlLibraryName(), entry.getResource().getId()))
+        .findFirst();
+  }
+
+  /**
+   * @param r5Measure retrieved from measure bundle
+   * @param library measure library
+   * @param accessToken used by MadieLibrarySourceProvider to make calls to madie-fhir-services
+   * @return effective data requirement of type R5 library
+   */
+  public org.hl7.fhir.r5.model.Library getEffectiveDataRequirements(
+      org.hl7.fhir.r5.model.Measure r5Measure, Library library, String accessToken) {
+    Attachment attachment =
+        library.getContent().stream()
+            .filter(content -> "text/cql".equals(content.getContentType()))
+            .findFirst()
+            .orElse(null);
+
+    if (attachment == null) {
+      log.error("Unable to find CQL text in library resource for library {} ", library.getId());
+      throw new ResourceNotFoundException("Library", library.getId());
+    }
+    String cql = new String(attachment.getData());
+
+    // setting up the librarySourceProvider to fetch included libraries
+    cqlConversionService.setUpLibrarySourceProvider(cql, accessToken);
+
+    var translationResource = TranslationResource.getInstance(true);
+    CqlTranslator cqlTranslator =
+        translationResource.buildTranslator(
+            new ByteArrayInputStream(cql.getBytes()), createDefaultRequestData().createMap());
+    CompiledLibrary translatedLibrary = cqlTranslator.getTranslatedLibrary();
+    LibraryManager libraryManager = translationResource.getLibraryManager();
+
+    Set<String> expressionList = getExpressions(r5Measure);
+    var dqReqTrans = new DataRequirementsProcessor();
+    CqlTranslatorOptions options = CqlTranslatorOptions.defaultOptions();
+
+    org.hl7.fhir.r5.model.Library effectiveDataRequirements =
+        dqReqTrans.gatherDataRequirements(
+            libraryManager, translatedLibrary, options, expressionList, true);
+    effectiveDataRequirements.setContent(createAttachment(library.getContent()));
+    effectiveDataRequirements.setId("effective-data-requirements");
+    return effectiveDataRequirements;
+  }
+
+  private Extension createExtension() {
+    var extension = new Extension();
+    extension.setUrl(EFFECTIVE_DATA_REQUIREMENT_URL);
+    //    extension.getValueReference().addChild("#effective-data-requirements");
+    return extension;
+  }
+
+  private List<org.hl7.fhir.r5.model.Attachment> createAttachment(List<Attachment> r4attachments) {
+    return r4attachments.stream()
+        .map(
+            attachment ->
+                new org.hl7.fhir.r5.model.Attachment()
+                    .setContentType(attachment.getContentType())
+                    .setData(attachment.getData()))
+        .collect(Collectors.toList());
+  }
+
+  private Set<String> getExpressions(org.hl7.fhir.r5.model.Measure r5Measure) {
+    Set<String> expressionSet = new HashSet<>();
+    r5Measure
+        .getSupplementalData()
+        .forEach(supData -> expressionSet.add(supData.getCriteria().getExpression()));
+    r5Measure
+        .getGroup()
+        .forEach(
+            groupMember -> {
+              groupMember
+                  .getPopulation()
+                  .forEach(
+                      population -> expressionSet.add(population.getCriteria().getExpression()));
+              groupMember
+                  .getStratifier()
+                  .forEach(
+                      stratifier -> expressionSet.add(stratifier.getCriteria().getExpression()));
+            });
+    return expressionSet;
   }
 }
