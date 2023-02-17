95a70de6a2711ccd760c008874fe31c1a935c3b3
Rohit Kandimalla <rohit.kandimalla@semanticbits.com>
2023-02-17 08:34:03 -0500

    Added test cases for library resource

--- src/test/java/gov/cms/mat/cql_elm_translation/service/HumanReadableServiceTest.java
+++ src/test/java/gov/cms/mat/cql_elm_translation/service/HumanReadableServiceTest.java
@@ -7,6 +7,8 @@ import gov.cms.madie.models.common.Version;
 import gov.cms.madie.models.measure.Measure;
 import gov.cms.madie.models.measure.MeasureMetaData;
 import gov.cms.mat.cql_elm_translation.exceptions.ResourceNotFoundException;
+import gov.cms.mat.cql_elm_translation.utils.ResourceUtils;
+import org.hl7.fhir.r4.model.Attachment;
 import org.hl7.fhir.r4.model.Bundle;
 import org.hl7.fhir.r4.model.Library;
 import org.hl7.fhir.r4.model.Period;
@@ -22,7 +24,7 @@ import java.util.Date;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class HumanReadableServiceTest {
@@ -33,9 +35,15 @@ class HumanReadableServiceTest {
 
   @Mock JsonParser jsonParser;
 
+  @Mock CqlConversionService cqlConversionService;
+
   @InjectMocks HumanReadableService humanReadableService;
 
   private Measure madieMeasure;
+
+  private org.hl7.fhir.r4.model.Measure measure;
+
+  private Library library;
 
   private final String testAccessToken = "test_access_token";
 
@@ -57,8 +65,7 @@ class HumanReadableServiceTest {
                     .build())
             .build();
 
-    org.hl7.fhir.r4.model.Measure measure = new org.hl7.fhir.r4.model.Measure();
-    measure
+    measure = new org.hl7.fhir.r4.model.Measure()
         .setName(madieMeasure.getCqlLibraryName())
         .setTitle(madieMeasure.getMeasureName())
         .setExperimental(true)
@@ -69,14 +76,15 @@ class HumanReadableServiceTest {
                 madieMeasure.getMeasurementPeriodStart(), madieMeasure.getMeasurementPeriodEnd()))
         .setCopyright(madieMeasure.getMeasureMetaData().getCopyright())
         .setDisclaimer(madieMeasure.getMeasureMetaData().getDisclaimer());
-    Bundle.BundleEntryComponent bundleEntryComponent = getBundleEntryComponent(measure);
-    Bundle bundle =
-        new Bundle().setType(Bundle.BundleType.TRANSACTION).addEntry(bundleEntryComponent);
+
+
+    String cqlData = ResourceUtils.getData("/cv_populations.cql");
+    library = new Library().addContent(new Attachment().setData(cqlData.getBytes()).setContentType("text/cql"));
+    library.setId("Library/" + madieMeasure.getCqlLibraryName());
 
     when(madieFhirServices.getFhirMeasureBundle(any(), anyString()))
         .thenReturn("bundleResourceJson");
     when(fhirContext.newJsonParser()).thenReturn(jsonParser);
-    when(jsonParser.parseResource(any(), anyString())).thenReturn(bundle);
   }
 
   public Bundle.BundleEntryComponent getBundleEntryComponent(Resource resource) {
@@ -91,10 +99,21 @@ class HumanReadableServiceTest {
 
   @Test
   void generateHumanReadable() {
+    Bundle.BundleEntryComponent measureBundleEntryComponent = getBundleEntryComponent(measure);
+    Bundle.BundleEntryComponent libraryBundleEntryComponent = getBundleEntryComponent(library);
+    Bundle bundle = new Bundle()
+        .setType(Bundle.BundleType.TRANSACTION)
+        .addEntry(measureBundleEntryComponent)
+        .addEntry(libraryBundleEntryComponent);
+
+    doNothing().when(cqlConversionService).setUpLibrarySourceProvider(anyString(), anyString());
+    when(jsonParser.parseResource(any(), anyString())).thenReturn(bundle);
+
     String generatedHumanReadable =
         humanReadableService.generateHumanReadable(madieMeasure, testAccessToken);
     assertNotNull(generatedHumanReadable);
     assertTrue(generatedHumanReadable.contains("test_measure_name"));
+    verify(cqlConversionService, times(1)).setUpLibrarySourceProvider(anyString(), anyString());
   }
 
   @Test
@@ -119,6 +138,38 @@ class HumanReadableServiceTest {
         new Bundle()
             .setType(Bundle.BundleType.TRANSACTION)
             .addEntry(getBundleEntryComponent(new Library()));
+    when(jsonParser.parseResource(any(), anyString())).thenReturn(bundle);
+    assertThrows(
+        ResourceNotFoundException.class,
+        () -> humanReadableService.generateHumanReadable(madieMeasure, testAccessToken));
+  }
+
+  @Test
+  void generateHumanReadableThrowsResourceNotFoundExceptionForNoMeasureLibraryResource() {
+    Bundle.BundleEntryComponent measureBundleEntryComponent = getBundleEntryComponent(measure);
+    Bundle bundle = new Bundle()
+        .setType(Bundle.BundleType.TRANSACTION)
+        .addEntry(measureBundleEntryComponent);
+    when(jsonParser.parseResource(any(), anyString())).thenReturn(bundle);
+    assertThrows(
+        ResourceNotFoundException.class,
+        () -> humanReadableService.generateHumanReadable(madieMeasure, testAccessToken));
+  }
+
+  @Test
+  void generateHumanReadableThrowsResourceNotFoundExceptionWhenCqlAttachmentIsNull() {
+    library.getContent().replaceAll(attachment -> attachment.setContentType(null));
+    library.setId("Library/" + madieMeasure.getCqlLibraryName());
+
+    Bundle.BundleEntryComponent measureBundleEntryComponent = getBundleEntryComponent(measure);
+    Bundle.BundleEntryComponent libraryBundleEntryComponent = getBundleEntryComponent(library);
+    Bundle bundle = new Bundle()
+        .setType(Bundle.BundleType.TRANSACTION)
+        .addEntry(measureBundleEntryComponent)
+        .addEntry(libraryBundleEntryComponent);
+
+    when(jsonParser.parseResource(any(), anyString())).thenReturn(bundle);
+
     when(jsonParser.parseResource(any(), anyString())).thenReturn(bundle);
     assertThrows(
         ResourceNotFoundException.class,
