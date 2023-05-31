578d5e3b13fd51bbb688de0c46102609d4b8e3fc
adongare <ashok.dongare@semanticbits.com>
2023-05-31 16:53:07 -0400

    MAT-5567 lowering the coverage to generate codecov report

--- src/test/java/gov/cms/mat/cql_elm_translation/service/HumanReadableServiceTest.java
+++ src/test/java/gov/cms/mat/cql_elm_translation/service/HumanReadableServiceTest.java
@@ -3,18 +3,15 @@ package gov.cms.mat.cql_elm_translation.service;
 import freemarker.template.Configuration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class HumanReadableServiceTest {
   @Mock Configuration freemarkerConfiguration;
 
   @BeforeEach
-  void setUp(){
-    //madie measure
+  void setUp() {
+    // MADiE measure
   }
 }
