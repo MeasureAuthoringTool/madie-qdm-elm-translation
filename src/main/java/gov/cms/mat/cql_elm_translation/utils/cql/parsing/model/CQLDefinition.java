a3aeefa5bd5f721cb3821c3a13087f48287fadad
Joseph Kotanchik <joseph.kotanchik@semanticbits.com>
2023-11-16 13:52:12 -0500

    MAT-6075: Lombak constructors and equals override for unit testing.
    Equal override relies on continuing to label CQL Definitions with their library prefix so we can utilize the CQL rule that Definitions within the same library cannot have the same name.
    
    Hence, two CQL Definition objects are considered equal, within the same library, if they have the same name.

--- src/main/java/gov/cms/mat/cql_elm_translation/utils/cql/parsing/model/CQLDefinition.java
+++ src/main/java/gov/cms/mat/cql_elm_translation/utils/cql/parsing/model/CQLDefinition.java
@@ -1,8 +1,16 @@
 package gov.cms.mat.cql_elm_translation.utils.cql.parsing.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Objects;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CQLDefinition implements CQLExpression {
   private String id;
   private String definitionName;
@@ -61,5 +69,13 @@ public class CQLDefinition implements CQLExpression {
   @Override
   public String toString() {
     return this.definitionName;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    CQLDefinition that = (CQLDefinition) o;
+    return Objects.equals(definitionName, that.definitionName);
   }
 }
