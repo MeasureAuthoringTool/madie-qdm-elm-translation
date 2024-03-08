babf8ffe5750494a1e0e5dfbf310d2659232e051
adongare <ashok.dongare@semanticbits.com>
2024-03-08 12:24:25 -0500

    MAT-6901 add UUID for each definition

--- src/main/java/gov/cms/mat/cql_elm_translation/utils/cql/parsing/model/CQLDefinition.java
+++ src/main/java/gov/cms/mat/cql_elm_translation/utils/cql/parsing/model/CQLDefinition.java
@@ -14,6 +14,7 @@ import java.util.Objects;
 @NoArgsConstructor
 public class CQLDefinition implements CQLExpression {
   private String id;
+  private String uuid;
   private String definitionName;
   private String definitionLogic;
   private String context = "Patient";
