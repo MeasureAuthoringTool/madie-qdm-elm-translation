f2c478825d3221e6717acfc9d936b97d15db03e7
nmorasb <nate.moraca@semanticbits.com>
2024-04-05 13:14:44 -0400

    MAT-7008: negation rationale update to fix relevant elements descriptions

--- src/main/java/gov/cms/mat/cql_elm_translation/utils/cql/QdmDatatypeUtil.java
+++ src/main/java/gov/cms/mat/cql_elm_translation/utils/cql/QdmDatatypeUtil.java
@@ -1,54 +1,59 @@
 package gov.cms.mat.cql_elm_translation.utils.cql;
 
+import gov.cms.mat.cql_elm_translation.data.DataElementDescriptor;
+
 import java.util.Map;
 
 public class QdmDatatypeUtil {
-
-  private static Map<String, String> negationRationale;
+  private static final Map<String, DataElementDescriptor> negationRationale;
 
   static {
-    negationRationale =
-        Map.ofEntries(
-            Map.entry("Assessment, Not Ordered", "AssessmentOrder"),
-            Map.entry(
-                "Assessment, Not Recommended", "AssessmentRecommended"), // Assessment, Recommended
-            Map.entry("Assessment, Not Performed", "AssessmentPerformed"),
-            Map.entry("Communication, Not Performed", "CommunicationPerformed"),
-            Map.entry("Device, Not Ordered", "DeviceOrder"),
-            Map.entry("Device, Not Recommended", "DeviceRecommended"),
-            Map.entry("Diagnostic Study, Not Ordered", "DiagnosticStudyOrder"),
-            Map.entry("Diagnostic Study, Not Recommended", "DiagnosticStudyRecommended"),
-            Map.entry("Diagnostic Study, Not Performed", "DiagnosticStudyPerformed"),
-            Map.entry("Encounter, Not Ordered", "EncounterOrder"),
-            Map.entry("Encounter, Not Recommended", "EncounterRecommended"),
-            Map.entry("Intervention, Not Ordered", "InterventionOrder"),
-            Map.entry("Intervention, Not Recommended", "InterventionRecommended"),
-            Map.entry("Intervention, Not Performed", "InterventionPerformed"),
-            Map.entry("Immunization, Not Ordered", "ImmunizationOrder"),
-            Map.entry("Immunization, Not Administered", "ImmunizationAdministered"),
-            Map.entry("Medication, Not Administered", "MedicationAdministered"),
-            Map.entry("Medication, Not Dispensed", "MedicationDispensed"),
-            Map.entry("Medication, Not Discharged", "MedicationDischarge"),
-            Map.entry("Medication, Not Ordered", "MedicationOrder"),
-            Map.entry("Physical Exam, Not Ordered", "PhysicalExamOrder"),
-            Map.entry("Physical Exam, Not Recommended", "PhysicalExamRecommended"),
-            Map.entry("Physical Exam, Not Performed", "PhysicalExamPerformed"),
-            Map.entry("Procedure, Not Ordered", "ProcedureOrder"),
-            Map.entry("Procedure, Not Recommended", "ProcedureRecommended"),
-            Map.entry("Procedure, Not Performed", "ProcedurePerformed"),
-            Map.entry("Substance, Not Ordered", "SubstanceOrder"),
-            Map.entry("Substance, Not Recommended", "SubstanceRecommended"),
-            Map.entry("Substance, Not Administered", "SubstanceAdministered"),
-            Map.entry("Laboratory Test, Not Ordered", "LaboratoryTestOrder"),
-            Map.entry("Laboratory Test, Not Recommended", "LaboratoryTestRecommended"),
-            Map.entry("Laboratory Test, Not Performed", "LaboratoryTestPerformed"));
+    negationRationale = Map.ofEntries(
+        Map.entry("Assessment, Not Ordered", new DataElementDescriptor("AssessmentOrder", "Assessment, Order")),
+        Map.entry(
+            "Assessment, Not Performed", new DataElementDescriptor("AssessmentPerformed", "Assessment, Performed")),
+        Map.entry(
+            "Assessment, Not Recommended", new DataElementDescriptor("AssessmentRecommended", "Assessment, Recommended")),
+        Map.entry("Communication, Not Performed", new DataElementDescriptor("CommunicationPerformed", "Communication, Performed")),
+        Map.entry("Device, Not Ordered",  new DataElementDescriptor("DeviceOrder", "Device, Order")),
+        Map.entry("Device, Not Recommended" , new DataElementDescriptor("DeviceRecommended", "Device, Recommended")),
+        Map.entry("Diagnostic Study, Not Ordered", new DataElementDescriptor("DiagnosticStudyOrder", "Diagnostic Study, Order")),
+        Map.entry("Diagnostic Study, Not Performed", new DataElementDescriptor("DiagnosticStudyPerformed", "Diagnostic Study, Performed")),
+        Map.entry("Diagnostic Study, Not Recommended", new DataElementDescriptor("DiagnosticStudyRecommended", "Diagnostic Study, Recommended")),
+        Map.entry("Encounter, Not Ordered", new DataElementDescriptor("EncounterOrder", "Encounter, Order")),
+        Map.entry("Encounter, Not Recommended", new DataElementDescriptor("EncounterRecommended", "Encounter Recommended")),
+        Map.entry("Intervention, Not Ordered", new DataElementDescriptor("InterventionOrder", "Intervention, Order")),
+        Map.entry("Intervention, Not Performed", new DataElementDescriptor("InterventionPerformed", "Intervention, Performed")),
+        Map.entry("Intervention, Not Recommended", new DataElementDescriptor("InterventionRecommended", "Intervention, Recommended")),
+        Map.entry("Immunization, Not Ordered", new DataElementDescriptor("ImmunizationOrder", "Immunization, Order")),
+        Map.entry("Immunization, Not Administered", new DataElementDescriptor("ImmunizationAdministered", "Immunization, Administered")),
+        Map.entry("Laboratory Test, Not Ordered", new DataElementDescriptor("LaboratoryTestOrder", "Laboratory Test, Order")),
+        Map.entry("Laboratory Test, Not Performed", new DataElementDescriptor("LaboratoryTestPerformed", "Laboratory Test, Performed")),
+        Map.entry("Laboratory Test, Not Recommended", new DataElementDescriptor("LaboratoryTestRecommended", "Laboratory Test, Recommended")),
+        Map.entry("Medication, Not Administered", new DataElementDescriptor("MedicationAdministered", "Medication, Administered")),
+        Map.entry("Medication, Not Discharged", new DataElementDescriptor("MedicationDischarge", "Medication, Discharge")),
+        Map.entry("Medication, Not Dispensed", new DataElementDescriptor("MedicationDispensed", "Medication, Dispensed")),
+        Map.entry("Medication, Not Ordered", new DataElementDescriptor("MedicationOrder", "Medication, Order")),
+        Map.entry("Physical Exam, Not Ordered", new DataElementDescriptor("PhysicalExamOrder", "Physical Exam, Order")),
+        Map.entry("Physical Exam, Not Performed", new DataElementDescriptor("PhysicalExamPerformed", "Physical Exam, Performed")),
+        Map.entry("Physical Exam, Not Recommended", new DataElementDescriptor("PhysicalExamRecommended", "Physical Exam, Recommended")),
+        Map.entry("Procedure, Not Ordered", new DataElementDescriptor("ProcedureOrder", "Procedure, Order")),
+        Map.entry("Procedure, Not Performed", new DataElementDescriptor("ProcedurePerformed", "Procedure, Performed")),
+        Map.entry("Procedure, Not Recommended", new DataElementDescriptor("ProcedureRecommended", "Procedure, Recommended")),
+        Map.entry("Substance, Not Administered", new DataElementDescriptor("SubstanceAdministered", "Substance, Administered")),
+        Map.entry("Substance, Not Ordered", new DataElementDescriptor("SubstanceOrder", "Substance, Order")),
+        Map.entry("Substance, Not Recommended", new DataElementDescriptor("SubstanceRecommended", "Substance, Recommended"))
+
+
+
+    );
   }
 
   public static boolean isValidNegation(final String negation) {
     return negationRationale.containsKey(negation);
   }
 
-  public static String getTypeForNegation(final String negation) {
+  public static DataElementDescriptor getDescriptorForNegation(final String negation) {
     return negationRationale.get(negation);
   }
 }
