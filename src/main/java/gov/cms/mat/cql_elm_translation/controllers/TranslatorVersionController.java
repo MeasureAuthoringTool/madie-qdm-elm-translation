57968f325a1da15bc6ea6672544fd79da8a67a31
nmorasb <nate.moraca@semanticbits.com>
2025-07-29 10:42:01 -0400

    MAT-8696

--- src/main/java/gov/cms/mat/cql_elm_translation/controllers/TranslatorVersionController.java
+++ src/main/java/gov/cms/mat/cql_elm_translation/controllers/TranslatorVersionController.java
@@ -1,5 +1,7 @@
 package gov.cms.mat.cql_elm_translation.controllers;
 
+import gov.cms.mat.cql_elm_translation.service.VersionService;
+import org.cqframework.cql.cql2elm.CqlTranslator;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +9,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import gov.cms.mat.cql_elm_translation.config.TranslatorVersionConfig;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.server.ResponseStatusException;
+
 
 @Slf4j
 @RestController
@@ -17,20 +20,20 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/translator-version")
 public class TranslatorVersionController {
 
-  private TranslatorVersionConfig translatorVersionConfig;
+  private VersionService versionService;
 
   @GetMapping()
   public ResponseEntity<String> getTranslatorVersion(
-      @RequestParam(required = true, name = "draft") boolean draft) {
-    log.info(
-        "Current translator version: " + translatorVersionConfig.getCurrentTranslatorVersion());
-    log.info(
-        "Most recent translator version: "
-            + translatorVersionConfig.getMostRecentTranslatorVersion());
-    String result =
-        draft
-            ? translatorVersionConfig.getMostRecentTranslatorVersion()
-            : translatorVersionConfig.getCurrentTranslatorVersion();
-    return ResponseEntity.status(HttpStatus.OK).body(result);
+          @RequestParam(required = true, name = "draft") boolean draft) {
+    if (!draft) {
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Non-draft version is no longer supported.");
+    }
+
+    try {
+      return ResponseEntity.ok(versionService.getTranslatorVersion());
+    } catch (IllegalStateException e) {
+      return ResponseEntity.status(HttpStatus.FAILED_DEPENDENCY)
+          .body("Unable to determine translator version.");
+    }
   }
 }
