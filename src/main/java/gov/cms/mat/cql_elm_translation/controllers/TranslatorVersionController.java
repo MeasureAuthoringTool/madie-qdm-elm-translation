93b82c60c506ae6da83ddc814017d735e87dce13
nmorasb <nate.moraca@semanticbits.com>
2025-08-01 09:37:06 -0400

    MAT-8696: refactors for testability improvements

--- src/main/java/gov/cms/mat/cql_elm_translation/controllers/TranslatorVersionController.java
+++ src/main/java/gov/cms/mat/cql_elm_translation/controllers/TranslatorVersionController.java
@@ -1,6 +1,7 @@
 package gov.cms.mat.cql_elm_translation.controllers;
 
-import gov.cms.mat.cql_elm_translation.service.VersionService;
+import org.apache.commons.lang3.StringUtils;
+import org.cqframework.cql.cql2elm.CqlTranslator;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,8 +19,6 @@ import org.springframework.web.server.ResponseStatusException;
 @RequestMapping("/translator-version")
 public class TranslatorVersionController {
 
-  private VersionService versionService;
-
   @GetMapping()
   public ResponseEntity<String> getTranslatorVersion(
       @RequestParam(required = true, name = "draft") boolean draft) {
@@ -28,11 +27,17 @@ public class TranslatorVersionController {
           HttpStatus.BAD_REQUEST, "Non-draft version is no longer supported.");
     }
 
-    try {
-      return ResponseEntity.ok(versionService.getTranslatorVersion());
-    } catch (IllegalStateException e) {
+    Package translatorPackage = getTranslatorPackage();
+    if (translatorPackage != null
+        && StringUtils.isNotBlank(translatorPackage.getImplementationVersion())) {
+      return ResponseEntity.ok(translatorPackage.getImplementationVersion());
+    } else {
       return ResponseEntity.status(HttpStatus.FAILED_DEPENDENCY)
           .body("Unable to determine translator version.");
     }
+  }
+
+  public Package getTranslatorPackage() {
+    return CqlTranslator.class.getPackage();
   }
 }
